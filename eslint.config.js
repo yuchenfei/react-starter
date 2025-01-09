88e2640feecf43aabb2422972492d83cc5af4880
yuchenfei <cf.yu@qq.com>
2025-01-10 00:21:05 +0800

    feat: add eslint-plugin-perfectionist for sorting

--- eslint.config.js
+++ eslint.config.js
@@ -1,11 +1,12 @@
 import js from '@eslint/js'
-import globals from 'globals'
+import jsxA11y from 'eslint-plugin-jsx-a11y'
+import perfectionist from 'eslint-plugin-perfectionist'
+import prettierRecommendedConfig from 'eslint-plugin-prettier/recommended'
 import react from 'eslint-plugin-react'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
-import jsxA11y from 'eslint-plugin-jsx-a11y'
+import globals from 'globals'
 import tseslint from 'typescript-eslint'
-import prettierRecommendedConfig from 'eslint-plugin-prettier/recommended'
 
 export default tseslint.config(
   { ignores: ['dist'] },
@@ -27,11 +28,83 @@ export default tseslint.config(
       'react-hooks': reactHooks,
       'react-refresh': reactRefresh,
     },
+    settings: {
+      react: {
+        version: 'detect',
+      },
+    },
     rules: {
       ...reactHooks.configs.recommended.rules,
       'react-refresh/only-export-components': [
         'warn',
         { allowConstantExport: true },
+      ],
+    },
+  },
+  {
+    extends: [js.configs.recommended, prettierRecommendedConfig],
+    plugins: {
+      perfectionist, // https://perfectionist.dev
+    },
+    settings: {
+      perfectionist: {
+        type: 'natural',
+      },
+    },
+    rules: {
+      'perfectionist/sort-named-exports': 'error',
+      'perfectionist/sort-named-imports': 'error',
+      'perfectionist/sort-exports': 'error',
+      'perfectionist/sort-imports': [
+        'error',
+        {
+          specialCharacters: 'trim',
+          internalPattern: ['^@/.*'],
+          groups: [
+            ['react-type', 'react'],
+            ['builtin-type', 'builtin'],
+            ['external-type', 'external'],
+            ['internal-type', 'internal'],
+            ['parent-type', 'parent'],
+            ['sibling-type', 'sibling'],
+            ['index-type', 'index'],
+            'object',
+            'unknown',
+          ],
+          customGroups: {
+            type: {
+              'react-type': '^react(.*)$',
+            },
+            value: {
+              react: '^react(.*)$',
+            },
+          },
+        },
+      ],
+      'perfectionist/sort-jsx-props': [
+        'error',
+        {
+          // https://codeguide.co/#attribute-order
+          groups: [
+            'top',
+            'class',
+            'data',
+            'unique',
+            'a11y',
+            'unknown',
+            'multiline',
+            'shorthand',
+            'callback',
+          ],
+          customGroups: {
+            top: ['^class$', '^id$', '^name$'],
+            class: ['^className$'],
+            data: ['^data-.+'],
+            unique: ['^src$', '^for$', '^type$', '^href$', '^value$', '^key$'],
+            a11y: ['^title$', '^alt$', '^role$', '^aria-.+'],
+            callback: '^on.+',
+          },
+        },
       ],
     },
   }
