35ee03f631c03c9d10289e757dd48c9dd7e917a1
yuchenfei <cf.yu@qq.com>
2025-01-09 16:22:40 +0800

    feat: add eslint-plugin-jsx-a11y

--- eslint.config.js
+++ eslint.config.js
@@ -3,6 +3,7 @@ import globals from 'globals'
 import react from 'eslint-plugin-react'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
+import jsxA11y from 'eslint-plugin-jsx-a11y'
 import tseslint from 'typescript-eslint'
 import prettierRecommendedConfig from 'eslint-plugin-prettier/recommended'
 
@@ -14,6 +15,7 @@ export default tseslint.config(
       ...tseslint.configs.recommended,
       react.configs.flat.recommended,
       react.configs.flat['jsx-runtime'],
+      jsxA11y.flatConfigs.recommended,
       prettierRecommendedConfig,
     ],
     files: ['**/*.{ts,tsx}'],
