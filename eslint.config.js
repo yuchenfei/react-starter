66eacf39e0520b550a03cd14fb103bf2bfb72548
yuchenfei <cf.yu@qq.com>
2025-01-09 15:09:41 +0800

    feat: add eslint-plugin-react

--- eslint.config.js
+++ eslint.config.js
@@ -1,5 +1,6 @@
 import js from '@eslint/js'
 import globals from 'globals'
+import react from 'eslint-plugin-react'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
 import tseslint from 'typescript-eslint'
@@ -11,6 +12,8 @@ export default tseslint.config(
     extends: [
       js.configs.recommended,
       ...tseslint.configs.recommended,
+      react.configs.flat.recommended,
+      react.configs.flat['jsx-runtime'],
       prettierRecommendedConfig,
     ],
     files: ['**/*.{ts,tsx}'],
