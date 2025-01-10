b5cf94ad200b3346988731f07dbc04463ab3fd1b
yuchenfei <cf.yu@qq.com>
2025-01-11 03:44:32 +0800

    feat: add tailwindcss

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,9 @@
+import tailwindcss from '@tailwindcss/vite'
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import tsconfigPaths from 'vite-tsconfig-paths'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tsconfigPaths()],
+  plugins: [react(), tsconfigPaths(), tailwindcss()],
 })
